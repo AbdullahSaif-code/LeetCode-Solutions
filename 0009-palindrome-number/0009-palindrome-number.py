1820cc5d4e78134b6606dc26a4e51f31fc8fc8cc
Abdullah_Saif <126707579+AbdullahaSaif@users.noreply.github.com>
2025-03-03 15:37:29 +0500

    Time: 10 ms (41.15%), Space: 17.5 MB (98.27%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        return x == x[::-1]
+        return str(x) == str(x)[::-1]
 x= 121
 sol = Solution()
 print(sol.isPalindrome(x))

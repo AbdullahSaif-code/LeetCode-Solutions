cbf956c3ccd73a70f8bc64c4bc013f993af5579c
Abdullah_Saif <126707579+AbdullahaSaif@users.noreply.github.com>
2025-03-03 15:58:29 +0500

    Time: 59 ms (8.51%), Space: 19 MB (30.57%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums)-1:
+            if nums[i] == nums[i+1]:
+                del nums[i]
+            else:
+                i += 1
+        return len(nums)
+List = [1,1,2]
+slo = Solution()
+print(slo.removeDuplicates(List))
